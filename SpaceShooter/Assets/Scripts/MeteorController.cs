4534dda6989d9698955fb9bfeeca5782be18b247
ReCeap <receap@web.de>
2018-10-01 15:07:36 +0200

    Play can now destory meteors

---
+++ SpaceShooter/Assets/Scripts/MeteorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorController : MonoBehaviour {
+
+    public float maxhealth = 200f;
+    public float currenthealth;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        currenthealth = maxhealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        destroymeteor();
+        maxhealthcap();
+    }
+
+    void destroymeteor()
+    {
+        if (currenthealth < 0)
+        {
+            currenthealth = 0;
+            Destroy(gameObject);
+        }
+    }
+    void maxhealthcap()
+    {
+        if (currenthealth > maxhealth)
+        {
+            currenthealth = maxhealth;
+        }
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/bulletController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+
+    public float bulletdamage;
+    public Playermovement player;
+
+    void Start()
+    {
+        GameObject pl = GameObject.FindGameObjectWithTag("Player");
+        player = pl.GetComponent<Playermovement>();
+    }
+
+    void Update()
+    {
+        changebulletdamage();
+    }
+
+    void changebulletdamage()
+    {
+        if (player.ActiveGun == "Gun00")
+        {
+            bulletdamage = 30f;
+        }
+        if (player.ActiveGun == "Gun01")
+        {
+            bulletdamage = 50f;
+        }
+        if (player.ActiveGun == "Gun02")
+        {
+            bulletdamage = 40f;
+        }
+        if (player.ActiveGun == "Gun03")
+        {
+            bulletdamage = 12.5f;
+        }
+        if (player.ActiveGun == "Gun04")
+        {
+            bulletdamage = 45f;
+        }
+        if (player.ActiveGun == "Gun05")
+        {
+            bulletdamage = 20f;
+        }
+        if (player.ActiveGun == "Gun06")
+        {
+            bulletdamage = 30f;
+        }
+        if (player.ActiveGun == "Gun07")
+        {
+            bulletdamage = 70f;
+        }
+        if (player.ActiveGun == "Gun08")
+        {
+            bulletdamage = 7.5f;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Player" || c.gameObject.tag == "Bullet")
+        {
+            Physics2D.IgnoreCollision(c.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+        else if (c.gameObject.tag == "Meteor")
+        {
+            c.gameObject.GetComponent<MeteorController>().currenthealth -= bulletdamage;
+            Destroy(gameObject);
+        }
+    }
+}
